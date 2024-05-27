ed75789e7d799feb6595dd56b3fe4ba055827e65
Jessica Zou <jeszou@student.unimelb.edu.au>
2024-05-27 20:36:45 +1000

    the filter and pop-up slider made

--- productliststyle.js
+++ productliststyle.js
@@ -1,8 +1,8 @@
 // Pop ups
-let popupButton = document.getElementById("popupButton");
-let valPopup1 = document.getElementById("valPopup1");
-let closeValpopup1 = document.getElementById("closeValpopup1");
-let chooseCard1 = document.getElementById("chooseCard1");
+let popupButton= document.getElementById("popupButton");
+let valPopup1= document.getElementById("valPopup1");
+let closeValpopup1= document.getElementById("closeValpopup1");
+let chooseCard1= document.getElementById("chooseCard1");
 
 popupButton.addEventListener('click', function() {
     console.log("about to add to "+valPopup1)
@@ -17,50 +17,78 @@ chooseCard1.addEventListener('click', function() {
     valPopup1.classList.remove('show');
 });
 
+//pop up image slider
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
 
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
-//filter
-
-let dropdownDisplay = document.querySelector('.select');
-let dropdownContent = document.querySelector('.menu');
-let arrow = document.querySelector('.arrow');
-let filterOptions = document.querySelectorAll('.text-option');
-let itemsContainer = document.getElementById('products');
-
-
-dropdownDisplay.addEventListener('click', () => {
-    let isDropdownOpen= dropdownContent.style.display=== 'block';
-    if (isDropdownOpen) {
-        dropdownContent.style.display = 'none';
-        arrow.classList.remove('arrow-down');
-        arrow.classList.add('arrow-right');
-    } else {
-        dropdownContent.style.display = 'block';
-        arrow.classList.remove('arrow-right');
-        arrow.classList.add('arrow-down');
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
     }
-});
+}
 
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
 
-filterOptions.forEach(option => {
-    option.addEventListener('click', filterItems);
-});
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
 
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
 
-function filterItems() {
-    //get class and then filter base on that
-    dropdownContent.style.display = 'none';
-    arrow.classList.remove('arrow-down');
-    arrow.classList.add('arrow-right');
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
 }
 
 
-window.addEventListener('click', function(event) {
+
+
+
+
+//filter
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdownDisplay= document.querySelector('.select');
+    const dropdownContent= document.querySelector('.menu');
+    const arrow= document.querySelector('.arrow');
+    const filterOptions= document.querySelectorAll('.text-option');
+    const products= document.getElementById('.products-list > div');
+
+
+dropdownDisplay.addEventListener('click', () => {
+    dropdownContent.style.display= dropdownContent.style.display=== "block" ? "none": "block";
+    arrow.classList.remove('arrow-down');
+    arrow.classList.add('arrow-right');
+    });
+
+function filterItems(filterValue) {
+    products.forEach(product => {
+        product.style.display= (filterValue=== "all" || product.getAttribute("data-category")=== filterValue) ? block: none
+    })
+}
+
+
+window.addEventListener('click', (event) => {
     if (!event.target.closest('.filter')) {
-        if (dropdownContent.style.display === 'block') {
-            dropdownContent.style.display = 'none';
+            dropdownContent.style.display= 'none';
             arrow.classList.remove('arrow-down');
             arrow.classList.add('arrow-right');
         }
-    }
+    });
 });
+
+
